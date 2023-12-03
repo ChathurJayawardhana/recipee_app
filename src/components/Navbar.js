a16be87da3daf967fb2a938cbc0abf8d77225900
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2023-12-03 11:04:06 +0530

    nav bar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import logo from '../assets/pizzaLogo.png'
 import { Link } from 'react-router-dom'
+import "../styles/Navbar.css";
 
 export default function Navbar() {
   return (
     <div className='navbar'>
       <div className='leftside'>
-            <img src={logo}/>
+            <img className='logo' src={logo}/>
       </div>
 
       <div className='rightside'>
-       <Link to='/'>Home</Link>
+        <div className='list'>
+        <Link to='/'>Home</Link>
        <Link to='/menu'>Menu</Link> 
        <Link to='/about'>About</Link>
        <Link to='/contact'>Contact</Link>
+        </div>
+      
       </div>
     </div>
   )
