e914fd828543964e5710181a5e95057281c77fd4
Chathura Jayawardhana <105495683+ChathurJayawardhana@users.noreply.github.com>
2023-12-03 09:48:23 +0530

    nav css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,9 @@ export default function Navbar() {
 
       <div className='rightside'>
        <Link to='/'>Home</Link>
+       <Link to='/menu'>Menu</Link> 
+       <Link to='/about'>About</Link>
+       <Link to='/contact'>Contact</Link>
       </div>
     </div>
   )
